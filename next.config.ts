acfdf9f000aa2b2ddf65b10a067750933d44b582
p5566123321 <p5566123321@gmail.com>
2024-11-12 22:47:19 +0800

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",  // <=== enables static exports
-  basePath: '/p5566123321.github.io',
   images: {
     unoptimized: true,
   },
