d8fd720ec46606845df393ec83d57c3822da2426
p5566123321 <p5566123321@gmail.com>
2024-11-16 19:31:19 +0800

    Initial commit

--- src/main/java/com/example/serving_web_content/ServingWebContentApplication.java
+++ src/main/java/com/example/serving_web_content/ServingWebContentApplication.java
@@ -3,7 +3,7 @@ package com.example.serving_web_content;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class ServingWebContentApplication {
 
 	public static void main(String[] args) {
